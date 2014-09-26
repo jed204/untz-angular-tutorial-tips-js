4c0ea1b2fbeca3d00312c455691c01a79ac87c3c
Jed Danner <jed.danner@gmail.com>
2014-09-26 00:55:08 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var app = angular.module('myApp', [
+	'untz.tutorialtips'
+]);
+
+app.controller('MainCtrl', ['$scope', 'untzTutorialTipsSvc',
+	 			   function($scope, untzTutorialTipsSvc)
+
+{
+	$scope.startTutorial = function() {
+		untzTutorialTipsSvc.setTips([ 
+			{ header: 'Welcome', text : 'Welcome to the first tip!', left: 450, top: 43 }, 
+			{ header: 'Clicked out?', text : 'You can restart this tutorial by clicking here.', left: 252, top: 164 },
+			{ header: 'Nice Header!', text : 'You can also point to the right side, if you want.', right: 443, top: 312, side: 'right' } 
+		]);
+	}
+
+	$scope.startTutorial();
+}]);
+
+
