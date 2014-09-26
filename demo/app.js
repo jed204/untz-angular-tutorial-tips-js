4daf0753ff019b9ca5303cadaf02e01e37f33368
John Danner <jdanner@intelemage.com>
2014-09-26 08:27:23 -0400

    update to locations

--- demo/app.js
+++ demo/app.js
@@ -11,8 +11,8 @@ app.controller('MainCtrl', ['$scope', 'untzTutorialTipsSvc',
 	$scope.startTutorial = function() {
 		untzTutorialTipsSvc.setTips([ 
 			{ header: 'Welcome', text : 'Welcome to the first tip!', left: 450, top: 43 }, 
-			{ header: 'Clicked out?', text : 'You can restart this tutorial by clicking here.', left: 252, top: 164 },
-			{ header: 'Nice Header!', text : 'You can also point to the right side, if you want.', right: 443, top: 312, side: 'right' } 
+			{ header: 'Clicked out?', text : 'You can restart this tutorial by clicking here.', left: 252, top: 134 },
+			{ header: 'Nice Header!', text : 'You can also point to the right side, if you want.', right: 443, top: 285, side: 'right' } 
 		]);
 	}
 
